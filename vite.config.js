2467ab54dbba7f3cc823945b4d0bf4f0ca41c5ba
Jona <Jona@DESKTOP-ACAM5N1>
2025-08-27 19:09:05 -0600

    ACTTTT

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        manualChunks: undefined
+      }
+    },
+    target: 'es2015',
+    minify: 'esbuild'
+  },
+  optimizeDeps: {
+    exclude: ['@rollup/rollup-linux-x64-gnu']
+  }
 })
